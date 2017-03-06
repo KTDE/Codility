e97a07e73afbc343cc954f89a76dbc4bd9de0243
TDKhoa <t.dinhkhoa@gmail.com>
2017-03-06 15:44:28 +0100

    Update FrogJmp.java

--- FrogJmp.java
+++ FrogJmp.java
@@ -1,12 +1,8 @@
 package codility;
 
 public class FrogJmp {
-	public static void main (String [] agrs)
-	{
-		System.out.println("number jumps is: " + solution(10,81,30));
-	}
 	
-	public static double solution (int X, int Y, int D)
+	public double solution (int X, int Y, int D)
 	{
 	//	int jumps = (int)(Math.ceil((double)(Y - X) / (double)( D)));
 		int jumps = (Y-X + (D-1))/D;
