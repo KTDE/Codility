6dbb043de36f0ccfd2c8094a24c5356c93315ae0
TDKhoa <t.dinhkhoa@gmail.com>
2017-03-06 15:50:15 +0100

    Update codlityTest.java

--- codlityTest.java
+++ codlityTest.java
@@ -5,30 +5,7 @@ import java.util.Scanner;
 
 public class codlityTest {
 	
-	public static void main (String [] args)
-	{
-		Scanner scanner = new Scanner (System.in);
-		System.out.println("Provide the test case: ");
-		double A [] = new double [3];
-		for (int i = 0; i<A.length;i++)
-		{
-			A[i] = scanner.nextDouble();
-		}
-		scanner.close();
-
-		// int A [] = {1,2,3,4,2,3,1};
-		System.out.println("The input array is: ");
-		
-		for (int i = 0; i<A.length; i++)
-		{
-			System.out.println(A[i]);
-		}
-		int valueE = solution (A);
-		System.out.println("\nThe equilibrium value is: " + valueE);
-
-	}
-	
-	public static int solution(double[] A)
+	public int solution(double[] A)
 	{	
 		int equilibrium = -2;
 		
@@ -48,8 +25,6 @@ public class codlityTest {
 					leftSum = leftSum + A[i];
 				}
 			}
-//			System.out.println("leftSum value is: ");
-//			System.out.println(leftSum);
 			
 			if (p == A.length-1)
 			{
@@ -62,21 +37,11 @@ public class codlityTest {
 					rightSum =rightSum + A[j];
 				}
 			}
-//			System.out.println("rightSum value is: ");
-//			System.out.println(rightSum);
 			
 			if (leftSum == rightSum)
 			{
-//				System.out.println(" This is equilibrium value: ");
-//				System.out.println(p);
 				equilibrium = p;
 			}
-//			else
-//			{
-//				equilibrium = -1;
-//				System.out.println("There is no Equilibrium");
-//			}
-		
 		}
 		if (equilibrium == 2)
 			{
