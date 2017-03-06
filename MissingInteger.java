f562f47b35e4cad604403a7cc2fe2fb5e3430944
TDKhoa <t.dinhkhoa@gmail.com>
2017-03-06 15:45:55 +0100

    Update MissingInteger.java

--- MissingInteger.java
+++ MissingInteger.java
@@ -2,12 +2,7 @@ package codility;
 
 public class MissingInteger {
 
-	public static void  main (String [] agrs)
-	{
-		int [] A = {-1,1,1,2,2,2,2,3,3,4,45,5, 2147483647};
-		System.out.println("Missing Value is " + solution(A));
-	}
-	public static int solution(int [] A)
+	public int solution(int [] A)
 	{
 		int n = A.length;
 		int [] count = new int [2147483647];
