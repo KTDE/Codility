bfa000d301d6e3c628ea1eb37b28a260f980fac1
TDKhoa <t.dinhkhoa@gmail.com>
2017-03-06 15:46:24 +0100

    Update NumberDiv.java

--- NumberDiv.java
+++ NumberDiv.java
@@ -1,11 +1,8 @@
 package codility;
 
 public class NumberDiv {
-	public static void main (String [] agrs)
-	{
-		System.out.println("number of divisible: " + solution(11,345,17));
-	}
-	public static int solution (int A, int B, int K)
+	
+	public int solution (int A, int B, int K)
 	{
 		// (A-1)/K is the sum of all the occurrences of the numbers <=(A-1) that are divisible by K.
 		// B/K is the sum of all the occurrences of the numbers <= B that are divisible by K.
